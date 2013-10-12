3b73eb9952c1e340a1a5ce1a18c84bc40160f495
Jordan Cortes <jordancg91@gmail.com>
2013-10-12 15:00:37 -0500

    Initial Commit

---
+++ Brother-Sister/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Brother-Sister
+//
+//  Created by Jordan Cortés Guzmán on 12/10/2013.
+//  Copyright (c) 2013 Ceci & Jordan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
